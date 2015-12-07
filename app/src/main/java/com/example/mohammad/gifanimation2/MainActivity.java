3d451fb1dd4067dc98fede4c2c5e23fd1da94757
MohammadTazehkar <mohammad.tazehkar@gmail.com>
2015-12-07 20:24:39 +0330

    show GIF

---
+++ app/src/main/java/com/example/mohammad/gifanimation2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mohammad.gifanimation2;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
